ce319821cfebff500cb0fe121cd4a8162a60c62f
Medjber Soundous <101836665+medjbersoundous@users.noreply.github.com>
2023-06-07 15:24:00 +0100

    Add files via upload

--- voyage/add.php
+++ voyage/add.php
@@ -1,120 +1,132 @@
 <?php
-
-
-
 include('config/connect.php');
 
- $nomville = $descville = $nompays = $nomcon = $gare = $hotel = $aeroport = '';
+$nomville = $descville = $nompays = $nomcon = $typenec = $nomnec = '';
 
-$errors = array('nomville' => '','descville' => '','nompays' => '', 'nomcon' => '', 'gare' => '', 'hotel' => '', 'aeroport' => '');/*on stockes les valeurs d'erreur dans cette array*/
+$errors = array(
+    'nomville' => '',
+    'descville' => '',
+    'nompays' => '',
+    'nomcon' => '',
+    'typenec' => '',
+    'nomnec' => '',
+);
 
-
-
- if (isset($_POST['submit'] )){
-
-  if (empty($_POST['nomville'])){
-     $errors['nomville'] ="enter une ville   <br/>";
-  }
-
-  }
-
-if (empty($_POST['descville'])){
-    $errors['descville'] = "Please enter a descville   <br/>";
-  }
-  else {
-    $descville = $_POST['descville'];
-    if(!preg_match('/^[a-zA-Z\s]+$/', $descville)){
-        $errors['descville'] = 'desc must be a valid desc'; 
+if (isset($_POST['submit'])) {
+    if (empty($_POST['nomville'])) {
+        $errors['nomville'] = "Entrez un nom de ville <br/>";
+    } else {
+        $nomville = $_POST['nomville'];
     }
-  }
+
+    if (empty($_POST['descville'])) {
+        $errors['descville'] = "Entrez une description de ville <br/>";
+    } else {
+        $descville = $_POST['descville'];
+        if (!preg_match('/^[a-zA-Z\s]+$/', $descville)) {
+            $errors['descville'] = 'La description doit être valide';
+        }
+    }
+
+    if (empty($_POST['nompays'])) {
+        $errors['nompays'] = "Entrez un nom de pays <br/>";
+    } else {
+        $nompays = $_POST['nompays'];
+    }
+
+    if (empty($_POST['nomcon'])) {
+        $errors['nomcon'] = "Entrez un nom de continent <br/>";
+    } else {
+        $nomcon = $_POST['nomcon'];
+    }
+
+    if (empty($_POST['typenec'])) {
+        $errors['typenec'] = "Entrez le nom d'une gare <br/>";
+    } else {
+        $typenec = $_POST['typenec'];
+    }
+
+    if (empty($_POST['nomnec'])) {
+        $errors['nomnec'] = "Entrez le nom necessaire <br/>";
+    } else {
+        $nomnec = $_POST['nomnec'];
+    }
+
+    if (!array_filter($errors)) {
+        $nomville = mysqli_real_escape_string($conn, $_POST['nomville']);
 
 
-
-if (empty($_POST['nompays'])){
-     $errors['nompays'] ="Please enter nompays   <br/>";
-  }
- 
-
-if (empty($_POST['nomcon'])){
-    $errors['nomcon'] ="Please enter nomcon   <br/>";
- }
- if (empty($_POST['gare'])){
-    $errors['gare'] ="Please enter gare   <br/>";
- }
- if (empty($_POST['hotel'])){
-    $errors['hotel'] ="Please enter hotel   <br/>";
- }
- if (empty($_POST['aeroport'])){
-    $errors['aeroport'] ="Please enter aeroport   <br/>";
- }
+        $descville = mysqli_real_escape_string($conn, $_POST['descville']);
+        $nompays = mysqli_real_escape_string($conn, $_POST['nompays']);
+        $nomcon = mysqli_real_escape_string($conn, $_POST['nomcon']);
+        $typenec = mysqli_real_escape_string($conn, $_POST['typenec']);
+        $nomnec = mysqli_real_escape_string($conn, $_POST['nomnec']);
 
 
-if (array_filter($errors)){
-    
-} else {
+        // Insertion dans la table "ville"
 
-   $nomville = mysqli_real_escape_string($conn,$_POST['nomville']);
-   $descville = mysqli_real_escape_string($conn,$_POST['descville']);
-   $nompays = mysqli_real_escape_string($conn,$_POST['nompays']);
-   $nomcon = mysqli_real_escape_string($conn,$_POST['nomcon']);
-   $gare = mysqli_real_escape_string($conn,$_POST['gare']);
-   $hotel = mysqli_real_escape_string($conn,$_POST['hotel']);
-   $aeroport = mysqli_real_escape_string($conn,$_POST['aeroport']);
+        $idpay = "SELECT idpays FROM pays where nompays ='$nompays';";
+                
+        $result = mysqli_query($conn, $idpay);
+        $voyages = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $a=$voyages[0]['idpays'];
 
 
-$sql = "INSERT INTO ville (nomville, descville ) VALUES ('$nomville', '$descville')";
-$sql2 = "INSERT INTO pays (nompays ) VALUES ('$nompays')";
-$sql3 = "INSERT INTO contient (nomcon ) VALUES ('$nomcon')";
-$sql4 = "INSERT INTO necessaire (gare, aeroport, hotel ) VALUES ('$gare', '$aeroport', '$hotel')";
-$sql5 = "INSERT INTO site (nomsite ) VALUES ('$nomsite')";
+        // if (empty($a) ){
+        //      // Insertion dans la table "pays"
+        // $sql2 = "INSERT INTO pays (nompays) VALUES ('$nompays')";
+        // mysqli_query($conn, $sql2);
+        // $paysId = mysqli_insert_id($conn); // Récupérer l'ID généré pour le pays inséré
+        // }
+        $sql = "INSERT INTO ville (nomville, descville,idpays) VALUES ('$nomville','$descville','$a');";
+        $conn->query($sql);
+        
+        $villeId =mysqli_insert_id($conn); // Récupérer l'ID généré pour la ville insérée
 
+        // Insertion dans la table "contient"
+        // $sql3 = "INSERT INTO contient (nomcon) VALUES ('$nomcon')";
+        // mysqli_query($conn, $sql3);
+        // $contientId = mysqli_insert_id($conn); // Récupérer l'ID généré pour le continent inséré
+        
+        // Insertion dans la table "necessaire"
+        $sql4 = "INSERT INTO necessaire (idville, typenec, nomnec) VALUES ( '$villeId','$typenec', '$nomnec');";
+        $conn->query($sql4);
+        // Insertion dans la table "site"
+       
 
-
-
-
-if (mysqli_query($conn,$sql, $sql2,$sql3, $sql4, $sql5 )){
-
- header('Location: index.php');
-}else {
- echo 'query error:' . mysqli_error($conn);
+        header('Location: index.php');
+    }
 }
-
-
-}
-
-
-
 
 ?>
+
 <html lang="en">
 <?php include('template/header.php'); ?>
 
 <section class="container grey-text">
-    <h4 class="center">ajouter une ville</h4>
+    <h4 class="center">Ajouter une ville</h4>
     <form class="white" action="add.php" method="post">
-        <label>nom de ville :</label>
-        <input type="text" name="email" value="<?php echo htmlspecialchars($nomville)  ?>">
-        <div class="red-text"><?php echo $errors['nomvi$nomville'] ?></div>
-        <label>description de ville:</label>
-        <input type="text" name="descville" value="<?php echo htmlspecialchars($descville)  ?>">
-        <div class="red-text"><?php echo $errors['desc$descville'] ?></div>
-        <label>pays:</label>
-        <input type="text" name="nompays" value="<?php echo htmlspecialchars($nompays)  ?>">
+        <label>Nom de la ville :</label>
+        <input type="text" name="nomville" value="<?php echo htmlspecialchars($nomville) ?>">
+        <div class="red-text"><?php echo $errors['nomville'] ?></div>
+        <label>Description de la ville :</label>
+        <input type="text" name="descville" value="<?php echo htmlspecialchars($descville) ?>"> 
+        <div class="red-text"><?php echo $errors['descville'] ?></div>
+        <label>Pays :</label>
+        <input type="text" name="nompays" value="<?php echo htmlspecialchars($nompays) ?>">
         <div class="red-text"><?php echo $errors['nompays'] ?></div>
-        <label>continent:</label>
-        <input type="text" name="nomcon" value="<?php echo htmlspecialchars($nomcon)  ?>">
+        <label>Continent :</label>
+        <input type="text" name="nomcon" value="<?php echo htmlspecialchars($nomcon) ?>">
         <div class="red-text"><?php echo $errors['nomcon'] ?></div>
-        <label>gare:</label>
-        <input type="text" name="gare" value="<?php echo htmlspecialchars($gare)  ?>">
-        <div class="red-text"><?php echo $errors['gare'] ?></div>
-        <label>aeroport:</label>
-        <input type="text" name="aeroport" value="<?php echo htmlspecialchars($aeroport)  ?>">
-        <div class="red-text"><?php echo $errors['aeroport'] ?></div>
-        <label>hotel:</label>
-        <input type="text" name="hotel" value="<?php echo htmlspecialchars($hotel)  ?>">
-        <div class="red-text"><?php echo $errors['hote$hotel'] ?></div>
+        <label>typenec :</label>
+        <input type="text" name="typenec" value="<?php echo htmlspecialchars($typenec) ?>">
+        <div class="red-text"><?php echo $errors['typenec'] ?></div>
+        <label>nomnec :</label>
+        <input type="text" name="nomnec" value="<?php echo htmlspecialchars($nomnec) ?>">
+        <div class="red-text"><?php echo $errors['nomnec'] ?></div>
+       
         <div class="center">
-            <input type="submit" value="submit" name="submit" class="btn brand z-depth-0">
+            <input type="submit" value="Submit" name="submit" class="btn brand z-depth-0">
         </div>
     </form>
 </section>
--- voyage/index.php
+++ voyage/index.php
@@ -1,78 +1,40 @@
-
 <?php
 include('config/connect.php');
 
+$sql = "SELECT ville.*, pays.nompays, continent.nomcon, necessaire.typenec, necessaire.nomnec
+        FROM ville
+        INNER JOIN pays ON ville.idpays = pays.idpays
+        INNER JOIN continent ON pays.idcon = continent.idcon
+        INNER JOIN necessaire ON ville.idville = necessaire.idville";
+        
+$result = mysqli_query($conn, $sql);
 
-$sql = "SELECT * FROM ville ";
-$sql2 = "SELECT * FROM pays ";
-$sql3 = "SELECT * FROM continent ";
-$sql4 = "SELECT * FROM necessaire ";
-$sql5 = "SELECT * FROM site ";
-
-
-
-$result = mysqli_query($conn,$sql, $sql2, $sql3, $sql4, $sql5 );
-
-
-$voyage = mysqli_fetch_all($result,MYSQLI_ASSOC);
-
+$voyages = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 mysqli_free_result($result);
-
-
 mysqli_close($conn);
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
-
 <?php include('template/header.php'); ?>
 <h4 class="center grey-text">travel</h4>
 <div class="container">
     <div class="row">
-        <?php foreach($voyage as $voyages): ?>
+        <?php foreach($voyages as $voyage): ?>
         <div class="col s6 md3">
             <div class="card z-depth-0">
                 <div class="card-content center">
-                    <h6><?php echo htmlspecialchars($voyages['descville']); ?></h6>
-                    <h6><?php echo htmlspecialchars($voyages['pays']); ?></h6>
-                    <h6><?php echo htmlspecialchars($voyages['continent']); ?></h6>
-                    
-                    <ul>
-                        <?php foreach(explode(',',$voyages['hotel'])as $ing): ?>
-                        <li> <?php echo htmlspecialchars($ing) ?></li>
-                        <?php  
-                         endforeach;
-                         ?>
-                    </ul>
-                    <ul>
-                        <?php foreach(explode(',',$voyages['gare'])as $ing): ?>
-                        <li> <?php echo htmlspecialchars($ing) ?></li>
-                        <?php  
-                         endforeach;
-                         ?>
-                    </ul>
-                    <ul>
-                        <?php foreach(explode(',',$voyages['aeroport'])as $ing): ?>
-                        <li> <?php echo htmlspecialchars($ing) ?></li>
-                        <?php  
-                         endforeach;
-                         ?>
-                    </ul>
-                    
+                    <h6><?php echo htmlspecialchars($voyage['descville']); ?></h6>
+                    <h6><?php echo htmlspecialchars($voyage['nompays']); ?></h6>
+                    <h6><?php echo htmlspecialchars($voyage['nomcon']); ?></h6>
+                    <h6><?php echo htmlspecialchars($voyage['typenec']); ?></h6>
+                    <h6><?php echo htmlspecialchars($voyage['nomnec']); ?></h6>
                 </div>
-                
             </div>
         </div>
-        <?php  endforeach; ?>
-
+        <?php endforeach; ?>
     </div>
 </div>
-<?php include('template/footer.php'); ?>
-<!---same for footer--->
-
 
 </html>
